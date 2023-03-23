75faee779313fbf7577c1c8bae9c0049c2638a5a
acidicMercury8 <acidicmercury8@outlook.com>
2023-03-23 21:06:20 +0300

    Add instances for `Maybe` type

--- src/ExpPrelude/Base/Maybe.hs
+++ src/ExpPrelude/Base/Maybe.hs
@@ -1,7 +1,23 @@
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# OPTIONS_GHC -Wno-redundant-constraints #-}
 
 module Base.Maybe
   ( Maybe (..)
   ) where
 
-data Maybe a = Nothing | Just a
+import GHC.Show (Show(show))
+import GHC.Base ((++))
+
+import Data.Eq (Eq)
+import Data.String (String)
+
+data Maybe a
+  = Nothing
+  | Just a
+  deriving (Eq)
+
+instance Show a => Show (Maybe a) where
+  show :: Show a => Maybe a -> String
+  show Nothing = "Nothing"
+  show (Just x) = "Just " ++ show x
